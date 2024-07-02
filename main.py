76fef1261d57784e681b38053abdd889982c4016
cj.tsai <jimtsai9596@gmail.com>
2024-07-02 08:10:58 +0800

    init

---
+++ main.py
@@ -0,0 +1,69 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+import random
+import string
+from bs4 import BeautifulSoup
+from getpass import *
+from os import listdir
+from os.path import join, isfile
+import sys
+
+n=len(sys.argv)
+if n!=3:
+    print("invalid arguments\nmain [problem_id] [code_file]")
+    exit()
+
+problem_id = sys.argv[1]
+
+file_name=sys.argv[2]
+
+compiler_id="11"
+
+if file_name.endswith("cpp"):
+    compiler_id="1"
+elif file_name.endswith("c"):
+    compiler_id="6"
+
+judge_site = 'https://iscoj.ckefgisc.org'
+
+session = requests.Session()
+def login():
+    TIOJusername = ""
+    TIOJpassword = ""
+    global session
+    rel = session.get(judge_site + '/users/sign_in')
+    soup = BeautifulSoup(rel.text, "html.parser")
+    inputs = soup.find('form').find_all('input')
+    rel = session.post(judge_site + '/users/sign_in', data = {
+        inputs[0].attrs['name']: inputs[0].attrs['value'],
+        #inputs[1].attrs['name']: inputs[1].attrs['value'],
+        'user[username]': TIOJusername,
+        'user[password]': TIOJpassword,
+        'user[remember_me]': '1',
+        'commit': 'Sign in'
+    })
+login()
+
+
+sign_up_get_url = judge_site + '/problems/%s/submissions/new' % problem_id
+sign_up_post_url = judge_site + '/problems/%s/submissions' % problem_id
+
+
+rel = session.get(sign_up_get_url)
+soup = BeautifulSoup(rel.text, "html.parser")
+inputs = soup.find('form').find_all('input')
+rel = session.post(sign_up_post_url, data = {
+        inputs[0].attrs['name']: inputs[0].attrs['value'],
+    'submission[compiler_id]':compiler_id,
+    'submission[code_content_attributes][code]': 'print(AaW大哥牛逼!)',
+    'commit': 'Create Submission'
+},files={
+'submission[code_file]':open(file_name,'rb')
+}
+)
+if "200" == str(rel).split("[")[1].split("]")[0]:
+    print("Operation success, waiting for judge result")
+else:
+    print("Operation failed")
