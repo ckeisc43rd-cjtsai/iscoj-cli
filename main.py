844da3f6151fca9e2e34b1fe9a1a847e52dca78d
cj.tsai <jimtsai9596@gmail.com>
2024-07-03 11:25:22 +0800

    update README.md

--- main.py
+++ main.py
@@ -42,27 +42,31 @@ def login():
         'user[password]': password,
         'user[remember_me]': '1',
         'commit': 'Sign in'
-    })
-login()
+        })
 
-
-sign_up_get_url = judge_site + '/problems/%s/submissions/new' % problem_id
-sign_up_post_url = judge_site + '/problems/%s/submissions' % problem_id
-
-
-rel = session.get(sign_up_get_url)
-soup = BeautifulSoup(rel.text, "html.parser")
-inputs = soup.find('form').find_all('input')
-rel = session.post(sign_up_post_url, data = {
-    inputs[0].attrs['name']: inputs[0].attrs['value'],
-    'submission[compiler_id]':compiler_id,
-    'submission[code_content_attributes][code]': '',
-    'commit': 'Create Submission'
-},files={
-    'submission[code_file]':open(file_name,'rb')
-}
-)
-
+try:
+    login()
+    sign_up_get_url = judge_site + '/problems/%s/submissions/new' % problem_id
+    sign_up_post_url = judge_site + '/problems/%s/submissions' % problem_id
+except:
+    print("login failed")
+    exit()
+try:
+    rel = session.get(sign_up_get_url)
+    soup = BeautifulSoup(rel.text, "html.parser")
+    inputs = soup.find('form').find_all('input')
+    rel = session.post(sign_up_post_url, data = {
+        inputs[0].attrs['name']: inputs[0].attrs['value'],
+        'submission[compiler_id]':compiler_id,
+        'submission[code_content_attributes][code]': '',
+        'commit': 'Create Submission'
+    },files={
+        'submission[code_file]':open(file_name,'rb')
+    }
+    )
+except:
+    print("Operation failed")
+    exit()
 if "200" == str(rel).split("[")[1].split("]")[0]:
     print("Operation success, waiting for judge result")
 else:
